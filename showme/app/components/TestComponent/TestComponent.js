4e3d8debf63b93ca0fa5ac9a927b0c3a3f881d53
Bilger Yahov <bilger@studyportals.com>
2017-11-01 16:57:03 +0100

    Initialize app structure.

---
+++ showme/app/components/TestComponent/TestComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text }  from 'react-native';
+
+const TestComponent = () => {
+	return (
+		<Text>Hello from TestComponent!</Text>
+	);
+};
+
+export default TestComponent;
---
+++ showme/app/screens/TestScreen.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+import { Text } from 'react-native';
+import { TestComponent } from '../components/TestComponent';
+
+class Contacts extends React.Component {
+
+	render() {
+		return (
+			<TestComponent/>
+		);
+	}
+}
+
+export default Contacts;
